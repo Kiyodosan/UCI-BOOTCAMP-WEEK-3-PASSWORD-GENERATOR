cb66aa6ca0f9c25adf02ee8d2940f40485e5cecd
Kiyodo <tyler.odo@gmail.com>
2023-10-15 18:33:35 -0700

    script.js first draft complete + initial testing passed

--- script.js
+++ script.js
@@ -1,27 +1,86 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Writes a password to the #password id.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  // Create a value for passwordText.value by using the generatePassword() function.
   passwordText.value = password;
 }
+
+// Adds event listener to generate button.
+generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   let pwCriteria = [];
   gatherCriteria(pwCriteria);
 
-  if (pwCriteria.length === 5) {
-    alert(pwCriteria.join(", "));     // Test for array input.
-  } /* else {
-    alert("Not enough criteria to generate password.");
+/*   if (pwCriteria.length === 5) {     /////////////////////////// Test for array input.
+    alert(pwCriteria.join(", "));
   } */
+
+  // Iterates through array to make decisions for password criteria.
+  let pwGen = makeRandomPw(pwCriteria);
+  return pwGen;
 }
 
-// Takes an empty "array" argument, then prompts the user to add criteria to the array.
+// Takes an array of 5 criteria elements to determine how to generate a random password.
+function makeRandomPw(arr) {
+  let criteria = [];
+  let randomCriteria = 5;   // Starts as '5' to check for errors.
+  let randomASCII = -1;     // Starts as '-1' to check for errors.
+  // Make a new array for special characters
+  let specialCharList = [33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 58, 59, 60, 61, 62, 63, 64, 91, 92, 93, 94, 95, 96, 123, 125, 126];
+  let newPw = [];
+
+  // Checks which criteria have been selected.
+  if (arr[1] === 'Y') {
+    criteria.push("lowercase");
+  }
+  if (arr[2] === 'Y') {
+    criteria.push("uppercase");
+  }
+  if (arr[3] === 'Y') {
+    criteria.push("numeric");
+  }
+  if (arr[4] === 'Y') {
+    criteria.push("special");
+  }
+  // console.log(arr[0]);     ////////////////////////// Testing if arr length is correct.
+
+  // Selects a random criteria, then selects a random character from that ASCII group.
+  for (let i = 0; i < arr[0]; i++) {
+    randomCriteria = Math.floor(Math.random() * criteria.length);
+    switch (criteria[randomCriteria]) {
+      case "lowercase": 
+        randomASCII = Math.floor((Math.random() * 26 + 97));
+        newPw.push(randomASCII);
+        break;
+      case "uppercase":
+        randomASCII = Math.floor((Math.random() * 26 + 65));
+        newPw.push(randomASCII);
+        break;
+      case "numeric":
+        randomASCII = Math.floor((Math.random() * 10 + 48));
+        newPw.push(randomASCII);
+        break;
+      case "special":
+        randomASCII = Math.floor((Math.random() * specialCharList.length));
+        newPw.push(specialCharList[randomASCII]);
+        break;
+    }
+  }
+  // alert(newPw.join(", "));     ///////////////////////// Test for unicode numbers.
+
+  // Converts integers from an array into ASCII characters for each element.
+  newPw = String.fromCharCode.apply(null, newPw);
+  // alert(newPw);     ///////////////////////// Test for ASCII characters.
+
+  return newPw;
+}
+
+// Takes an empty array as an argument, then prompts the user to add criteria to the array.
 function gatherCriteria(arr) {
   let promptLength;
   let promptLowerCase;
@@ -59,16 +118,36 @@ function gatherCriteria(arr) {
   } else {
     return;
   }
+
+  // Checks to see if at least one prompt has been selected.
+  let hasCriteria = false;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === 'Y') {
+      hasCriteria = true;
+      break;
+    }
+  }
+  if (!hasCriteria) {
+    alert("Please select at least one criteria.");
+    arr = [];
+  }
 }
 
 function pwLength() {
   let answer = prompt("How long should the password be?", "Choose between 8 and 128 characters.");
-  if (answer < 8 || answer > 128) {
+  if (answer >= 8 && answer <= 128) {
+    return answer;
+  } else {
+    alert("That is not a valid password length. Please try again.");
+    return false;
+  }
+
+/*   if (answer < 8 || answer > 128) {
     alert("That is not a valid password length. Please try again.");
     return false;
   } else {
     return answer;
-  }
+  } */
 }
 
 function includeLowerCase() {
@@ -79,13 +158,6 @@ function includeLowerCase() {
     alert("That is not a valid answer. Please try again.");
     return false;
   }
-
-/*     if (answer !== 'Y' || answer !== 'y' || answer !== 'N' || answer !== 'n') {
-      alert("That is not a valid answer. Please try again.");
-      return false;
-    } else {
-      return answer.toUpperCase();
-    } */
 }
 
 function includeUpperCase() { 
@@ -117,6 +189,3 @@ function includeSpecialChar() {
     return false;
   }
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
